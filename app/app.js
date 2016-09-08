15c359e2dd389f442b6aadc51e05924f514d4400
Mark Zachary <MarkZaWill@gmail.com>
2016-09-08 18:49:32 -0500

    Currently changing the Javascript SDK to work on an Angular app.  This has caused some gnashing of teeth. Stay tuned, true believer

---
+++ app/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*exported Wedding App*/
+
+let Wedding = angular.module("Wedding",['ngRoute']);
+
+/*Setting up the routes for the Wedding app*/
+
+Wedding.config(["$routeProvider",
+	function($routeProvider){
+		$routeProvider.
+
+		when("/", {
+			templateUrl: "partials/login.html",
+			// controller:"loginController"
+		}).
+		when("/main",{
+			templateUrl: "partials/main",
+			controller: "mainController"
+		}).
+		when("/twitter",{
+			templateUrl: "partials/twitter",
+			// controller: "twitterController"
+		});
+  }]);
